52b7bbfcd7136ee23fa998d613cbbc950192a491
c832026 <c832026@gmail.com>
2023-11-05 10:31:44 +0800

    Add a function and several variables

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(numA, numB, operator) {
+    switch (operator) {
+        case '+':
+            return add(numA, numB);
+            break;
+        case '-':
+            return subtract(numA, numB);
+            break;
+        case '*':
+            return multiply(numA, numB);
+            break;
+        case '/':
+            return divide(numA, numB);
+    }
+}
+
+let firstNumber, secondNumber, operator;
+
