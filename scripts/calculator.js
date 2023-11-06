980785473d3dc3064375c930e8cac077def2b495
c832026 <c832026@gmail.com>
2023-11-06 22:17:52 +0800

    Add all the functions to make the calculator works
    - Add the required basic arithmetic functions(Add, Subtract, Multiply, Divide)
    - Can handle floating number, round to five decimal places
    - A button to transform number to negative or positive
    - An all-clear button
    - An delete button to delete the last input(including decimal or operator)
    - Show in-time action and calculated result to display

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -7,14 +7,16 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Math.round((a * b) * (10 ** 5)) / (10 ** 5);
 }
 
 function divide(a, b) {
-    return a / b;
+    return Math.round((a / b) * (10 ** 5)) / (10 ** 5);
 }
 
 function operate(numA, numB, operator) {
+    numA = +numA;
+    numB = +numB;
     switch (operator) {
         case '+':
             return add(numA, numB);
@@ -22,28 +24,44 @@ function operate(numA, numB, operator) {
         case '-':
             return subtract(numA, numB);
             break;
-        case '*':
+        case '×':
             return multiply(numA, numB);
             break;
-        case '/':
+        case '÷':
             return divide(numA, numB);
     }
+}
+
+function showResult(numA, numB, theOperator) {
+    const result = operate(numA, numB, theOperator);
+    firstNumber = result.toString();
+    secondNumber = '';
+    operator = '';
 }
 
 let firstNumber = '', secondNumber = '', operator = '';
 const display = document.querySelector('.display-screen>span');
 
-// Set arithmetic logic using "Event delegation"
+// Set arithmetic logic with "Event delegation"
 const buttons = document.querySelector('.buttons-container');
 buttons.addEventListener('click', e => {
     if (e.target.classList.contains('button')) {
+        // Reset font size
+        display.style.fontSize = '48px';
+
         const btnText = e.target.textContent;
+
         // Get first number
         if (!operator && btnText >= 0 && btnText <= 9) {
             firstNumber += btnText;
+
         // Get operator
         } else if ((btnText === '×' || btnText === '÷' || btnText === '+' || btnText === '-') &&
-                  firstNumber.length != 0 && secondNumber.length === 0) {
+            firstNumber.length != 0) {
+            // Calculate and show result if already got two number in advance
+            if (secondNumber.length != 0) {
+                showResult(firstNumber, secondNumber, operator);
+            }
             switch (btnText) {
                 case '×':
                     operator = '×';
@@ -57,11 +75,55 @@ buttons.addEventListener('click', e => {
                 case '-':
                     operator = '-';
             }
+
         // Get second number
         } else if (operator && btnText >= 0 && btnText <= 9) {
             secondNumber += btnText;
+        } else if (btnText === 'AC') {
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+
+        // Delete the last input
+        } else if (btnText === 'DEL') {
+            if (secondNumber.length != 0) {
+                secondNumber = secondNumber.slice(0, -1);
+            } else if (operator && secondNumber.length === 0) {
+                operator = '';
+            } else if (!operator) {
+                firstNumber = firstNumber.slice(0, -1);
+            }
+        } else if (btnText === '+/-') {
+            if (operator && !secondNumber.includes('-')) {
+                secondNumber = '-' + secondNumber;
+            } else if (secondNumber.includes('-')) {
+                secondNumber = secondNumber.slice(1);
+            } else if (!operator && !firstNumber.includes('-')) {
+                firstNumber = '-' + firstNumber;
+            } else if (firstNumber.includes('-')) {
+                firstNumber = firstNumber.slice(1);
+            }
+        } else if (btnText === '.') {
+            if (!operator && !firstNumber.includes('.')) {
+                firstNumber += '.';
+            } else if (operator && !secondNumber.includes('.')) {
+                secondNumber += '.';
+            }
         }
-        
+
+        if (btnText === '=' && firstNumber.length != 0 && secondNumber.length != 0) {
+            showResult(firstNumber, secondNumber, operator);
+        }
+
         display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+
+        // Show error message if user tries divide by 0
+        if (firstNumber === 'Infinity' || firstNumber === '-Infinity') {
+            display.textContent = 'Error, can\'t divide number by 0 or -0';
+            display.style.fontSize = '35px';
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+        }
     }
 });
