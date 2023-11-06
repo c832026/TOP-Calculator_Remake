7dd45eaa2c5160a653871580e17653f6581639d6
c832026 <c832026@gmail.com>
2023-11-06 17:00:09 +0800

    Show display after click the buttons

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -30,5 +30,38 @@ function operate(numA, numB, operator) {
     }
 }
 
-let firstNumber, secondNumber, operator;
+let firstNumber = '', secondNumber = '', operator = '';
+const display = document.querySelector('.display-screen>span');
 
+// Set arithmetic logic using "Event delegation"
+const buttons = document.querySelector('.buttons-container');
+buttons.addEventListener('click', e => {
+    if (e.target.classList.contains('button')) {
+        const btnText = e.target.textContent;
+        // Get first number
+        if (!operator && btnText >= 0 && btnText <= 9) {
+            firstNumber += btnText;
+        // Get operator
+        } else if ((btnText === '×' || btnText === '÷' || btnText === '+' || btnText === '-') &&
+                  firstNumber.length != 0 && secondNumber.length === 0) {
+            switch (btnText) {
+                case '×':
+                    operator = '×';
+                    break;
+                case '÷':
+                    operator = '÷';
+                    break;
+                case '+':
+                    operator = '+';
+                    break;
+                case '-':
+                    operator = '-';
+            }
+        // Get second number
+        } else if (operator && btnText >= 0 && btnText <= 9) {
+            secondNumber += btnText;
+        }
+        
+        display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    }
+});
